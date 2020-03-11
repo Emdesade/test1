2e0c5bd1496a9c29ed0ec161423aaad5bfcfd68f
Emdesade <59286572+Emdesade@users.noreply.github.com>
2020-03-11 18:03:19 +0100

    Add files via upload

---
+++ ok.py
@@ -0,0 +1,109 @@
+#zad1
+#napis = input()
+#x = napis.count(" ")
+#print(x)
+#zad 2
+# import sys
+# print("podaj liczbe")
+# a = sys.stdin.readline()
+# print(a)
+# print("podaj 2 liczbe")
+# b = sys.stdin.readline()
+# print(b)
+# b=int(b)
+# wynik = a*b
+# print(wynik)
+#zad 4
+# from math import *
+# a = input("podaj liczbe")
+# a=int(a)
+# print(fabs(a))
+# zad 5
+# a = input("podaj liczbe")
+# a=int(a)
+# b= input("podaj 2 liczbe")
+# b=int(b)
+# c= input("podaj 3 liczbe")
+# c=int(c)
+# if a>10 and a>b or b>c:
+#     print("zawiera sie w przedziale ")
+# elif a<10 and b>a or c<b:
+#     print("niezawiera sie w przedziale")
+#zad 6
+# for x in range(1,26,1):
+#     if x%5==0:
+#         print(x)
+#zad 7
+# kwadrat =int(input("podaj liczbe"))
+# for i in range(1,kwadrat +1):
+#     print(i*i)
+# liczba = [1,2,3,4,5,6]
+# for i in range (0,6):
+#     print (liczba [i]**2)
+#zad 10
+# j=int(input("podaj ile # ma mieć największa choinka? "))
+# m=3
+# for i in range(0, j):
+#     for k in range(1, m):
+#         print(k * "A")
+#     m+=1
+#zad11
+
+
+# import  this
+# # pep20
+
+# imie = 'Marianna'
+# lista = []
+# for litera in imie:
+#     lista.append(litera.upper())
+# print([litera.upper() for litera in imie])
+# print([litera.upper() for litera in imie])
+#suma cyfr
+# liczba = 123456789
+# print(sum([int(cyfra))for cyfra in str(liczba)]))
+
+
+# print(lista)    
+# print([2**n for in range(5)])
+# lista = [
+#     [1,2],
+#     [3,4]]
+# print([element for wiersz in lista for element in wiersz if element %2 == 0])
+# imie = 'Marianna'
+# {para[0]:para[1] for para in enumerate (imie)}
+# slownik = {klucz:wartosc for klucz, wartosc in enumerate (imie)}
+# slownik[0]
+# print(slownik)
+# slow_odwr ={wartosc:klucz for klucz, wartosc in slownik.items()}
+# print(slow_odwr)
+
+# litery = set(imie)
+# print(litery)
+
+# imie, nazwisko =('Marian', 'Bąbel')
+
+# print({*range(9)})
+# from timeit import timeit
+# print(timeit("list()",number=100000))
+
+# def witaj(imie='Jan'):
+#     print(f'Witaj {imie}!')
+# witaj()
+# witaj("Arkadiusz")
+# # moduły i pakiety
+# import start as  s 
+# s.pow() 
+
+#slicing
+# imie = "Małgorzata"
+# print(imie[0])#pierwsza
+# print(imie[-1])#ostatnia
+
+
+
+
+
+
+
+
